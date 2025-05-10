2f228a6ff575c65182c76dacf69ad98859f0eb57
DhruvKaura <dhruvkaura25@gmail.com>
2025-05-10 19:29:26 +0530

    logo added

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logos/tripply-icon.png" sizes="any" />
+      </head>
       <body
         className={`${inter.className}`}
       >
