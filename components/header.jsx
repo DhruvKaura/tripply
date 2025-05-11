078455acaa9f73f24ff1b1b3105b8124ce351294
DhruvKaura <dhruvkaura25@gmail.com>
2025-05-12 02:06:09 +0530

    convex and clerk are now connected as database is storing users, usersjs and use-store-users

--- components/header.jsx
+++ components/header.jsx
@@ -1,17 +1,28 @@
+"use client";
+
+import { useStoreUser } from '@/hooks/use-store-user';
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import {BarLoader} from 'react-spinners';
 import React from 'react'
 
 const Header = () => {
+
+   const {isLoading} = useStoreUser();
   return (
-    <div>
-       <SignedOut>
+    <header className='fixed top-0 w-full border-b bg-white/95 backdrop-blur z-50 supports-[backdrop-filter]:bg-white/60
+    '>
+      <nav>
+               <SignedOut>
               <SignInButton />
               <SignUpButton />
             </SignedOut>
             <SignedIn>
               <UserButton />
             </SignedIn>
-    </div>
+      </nav>
+      {isLoading && <BarLoader width={"100%"} color='#A51BE1'
+      />}
+    </header>
   )
 }
 
