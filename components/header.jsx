5dbe7edaf29de10c0fd435edb627ee184717d44f
Dhruv Kaura <dhruvkaura25@gmail.com>
2025-05-16 11:20:27 +0530

    dashboard authentication done

--- components/header.jsx
+++ components/header.jsx
@@ -1,52 +1,92 @@
 "use client";
 
-import { useStoreUser } from '@/hooks/use-store-user';
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
-import {BarLoader} from 'react-spinners';
-import React from 'react'
-import Link from 'next/link';
-import Image from 'next/image';
-import { usePathname } from 'next/navigation';
+import { useStoreUser } from "@/hooks/use-store-user";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { BarLoader } from "react-spinners";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { Authenticated, Unauthenticated } from "convex/react";
+import { Button } from "./ui/button";
+import { LayoutDashboard } from "lucide-react";
 
 const Header = () => {
-
-   const {isLoading} = useStoreUser();
-   const path = usePathname();
+  const { isLoading } = useStoreUser();
+  const path = usePathname();
   return (
-    <header className='fixed top-0 w-full border-b bg-white/95 backdrop-blur z-50 supports-[backdrop-filter]:bg-white/60
-    '>
-      <nav className='container mx-auto px-4 h-16 flex items-center justify-between'>
-        <Link href="/" className='flex items-center gap-2'>
+    <header
+      className="fixed top-0 w-full border-b bg-white/95 backdrop-blur z-50 supports-[backdrop-filter]:bg-white/60
+    "
+    >
+      <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
+        <Link href="/" className="flex items-center gap-2">
           <Image
             src="/logos/logo.png"
             width={200}
             height={60}
             alt="Logo"
-            className='h-11 w-auto object-contain'
+            className="h-11 w-auto object-contain"
           />
         </Link>
 
         {path === "/" && (
-          <div className='hidden md:flex items-center gap-6'>
-            <Link 
+          <div className="hidden md:flex items-center gap-6">
+            <Link
               href="#features"
-              className='text-sm font-medium hover:text-purple-700 transition'
+              className="text-sm font-medium hover:text-purple-700 transition"
             >
               Features
             </Link>
-                        <Link 
+            <Link
               href="#how-it-works"
-              className='text-sm font-medium hover:text-purple-700 transition'
+              className="text-sm font-medium hover:text-purple-700 transition"
             >
               How It Works
             </Link>
           </div>
         )}
-      </nav>
-      {isLoading && <BarLoader width={"100%"} color='#A51BE1'
-      />}
-    </header>
-  )
-}
+        <div className="flex items-center gap-4">
+          {/* If the user is already signed */}
 
-export default Header
+          <Authenticated>
+            <Link href="/dashboard">
+              <Button variant="outline" className="hidden md:inline-flex items-center gap-2 hover:text-purple-600 hover:border-purple-600 transition">
+                <LayoutDashboard className="h-4 w-4"/>Dashboard
+                  {/* Dashboard Icon*/}
+              </Button>
+
+              {/*Button only on mobile screen*/ }
+              <Button variant={'ghost'} className="md:hidden w-10 h-10 p-0">
+                <LayoutDashboard className="w-4 h-4"/>
+              </Button>
+            </Link>
+          </Authenticated>
+
+          {/* If the user is not signed in yet */}
+          <Unauthenticated>
+            <SignInButton>
+              <Button className="cursor-pointer" variation={"ghost"}>
+                Sign In
+              </Button>
+            </SignInButton>
+            <SignUpButton>
+            <Button className=" bg-purple-600 hover:bg-purple-700 transition cursor-pointer">
+              Get Started
+            </Button>
+          </SignUpButton>
+          </Unauthenticated>
+        </div>
+      </nav>
+      {isLoading && <BarLoader width={"100%"} color="#A51BE1" />}
+    </header>
+  );
+};
+
+export default Header;
